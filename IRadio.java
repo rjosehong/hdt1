a810d7d5a947d467c1e549f056e0580af47c08a5
rjosehong <rodrigojosehong@gmail.com>
2026-01-16 09:24:30 -0600

    Initial commit

---
+++ IRadio.java
@@ -0,0 +1,11 @@
+package hojadetrabajo1.hdt1;
+
+public interface IRadio {
+        void prenderRadio();
+        void apagarRadio();
+        void avanzarEstacion();
+        void guardarEstacion(int numeroBoton);
+        void cargarEstacion(int numeroBoton);
+        void cambiarFM();
+        void cambiarAM();
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package hojadetrabajo1.hdt1;
+
+public class Main {
+    public static void main(String[] args){
+        
+    }
+}
