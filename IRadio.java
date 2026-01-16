fb40b21ce0ac5ecbc8705ad22c44718ea6850228
rodrigo <rodrigojosehong@gmail.com>
2026-01-16 10:01:53 -0600

    Add files via upload

--- IRadio.java
+++ IRadio.java
@@ -1,29 +1,11 @@
 package hojadetrabajo1.hdt1;
 
 public class IRadio {
-    private boolean estado;
-	private String frecuencia;
-	private double amEstacion;
-	private double fmEstacion;
-	private double[] botones;
-	
-	public boolean getEstado() {
-		return estado;
-	}
-	
-	public String getFrecuencia() {
-		return frecuencia;
-	}
-	
-	public double getAMEstacion() {
-		return amEstacion;
-	}
-	
-	public double getFMEstacion() {
-		return fmEstacion;
-	}
-	
-	public double[] getBotones() {
-		return botones;
-	}
+	void prenderRadio();
+    void apagarRadio();
+    void avanzarEstacion();
+    void guardarEstacion(int numeroBoton);
+    void cargarEstacion(int numeroBoton);
+    void cambiarFM();
+    void cambiarAM();
 }
